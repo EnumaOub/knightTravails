55d5059c5f7148b8e8dbb4d51431bb229f36cb21
EnumaOub <rayane.oubenali@gmail.com>
2024-07-16 20:07:14 +0200

    Build functions giving a list of knight moves possible for a set of coordinate

--- knightTravails.js
+++ knightTravails.js
@@ -9,3 +9,35 @@ class knightNode {
         this.path.concat(coordinate);
     }
 };
+
+function knightTravails () {
+    const forbiddenMovement = function(coord) {
+        return coord[0] < 8 && coord[1] < 8;
+    };
+
+    const knightMovement = function(coord) {
+        const movesPossible = [ 
+            [coord[0] + 2, coord[1] + 1],
+            [coord[0] + 2, coord[1] + -1],
+            [coord[0] + -2, coord[1] + 1],
+            [coord[0] + -2, coord[1] + -1],
+            [coord[0] + 1, coord[1] + 2],
+            [coord[0] + 1, coord[1] + -2],
+            [coord[0] + -1, coord[1] + 2],
+            [coord[0] + -1, coord[1] + -2],
+        ]
+        return movesPossible.filter((c) => forbiddenMovement(c));
+
+    };
+
+    const getPath = function(coordInit, coordFin) {
+
+    }
+
+    return {
+        knightMovement
+    }
+}
+
+const knightMvt = knightTravails()
+console.log(knightMvt.knightMovement([5,6]))
