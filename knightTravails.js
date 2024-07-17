71b7ede76c8716f67a5f512378f2a4ef1a71f3a2
EnumaOub <rayane.oubenali@gmail.com>
2024-07-17 19:47:46 +0200

    Function path init with check coordinates initial and final

--- knightTravails.js
+++ knightTravails.js
@@ -12,7 +12,7 @@ class knightNode {
 
 function knightTravails () {
     const forbiddenMovement = function(coord) {
-        return coord[0] < 8 && coord[1] < 8;
+        return coord[0] < 8 && coord[1] < 8 && coord[0] >= 0 && coord[1]  >= 0;
     };
 
     const knightMovement = function(coord) {
@@ -30,14 +30,32 @@ function knightTravails () {
 
     };
 
+    const checkCoordinate = function(coord) {
+        if (!forbiddenMovement(coord)) {
+            console.log(`The Coordinates: ${coord} are not right`);
+            return false;
+        };
+        return true;
+    };
+
     const getPath = function(coordInit, coordFin) {
+        if (!checkCoordinate(coordInit) || !checkCoordinate(coordFin)) {
+            return null;
+        }
+        else {
+            return knightMovement(coordInit);
+        }
+
 
     }
 
     return {
-        knightMovement
+        getPath
     }
 }
 
 const knightMvt = knightTravails()
-console.log(knightMvt.knightMovement([5,6]))
+console.log(knightMvt.getPath([5,6], [9,6]))
+console.log(knightMvt.getPath([5,15], [4,1]))
+console.log(knightMvt.getPath([5,15], [9,6]))
+console.log(knightMvt.getPath([5,6], [4,1]))
